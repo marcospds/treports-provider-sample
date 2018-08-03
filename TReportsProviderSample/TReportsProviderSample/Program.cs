b7cc77adfd9b3424722a500159dd7e438d6520ec
Daniel Santos Campos <daniel.campos@totvs.com.br>
2018-08-03 14:18:27 -0300

    Inserindo arquivo hosting.json

--- TReportsProviderSample/TReportsProviderSample/Program.cs
+++ TReportsProviderSample/TReportsProviderSample/Program.cs
@@ -10,16 +10,25 @@ using Microsoft.Extensions.Logging;
 
 namespace TReportsProviderSample
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+      BuildWebHost(args).Run();
     }
+
+    public static IWebHost BuildWebHost(string[] args)
+    {
+      var configuration = new ConfigurationBuilder()
+                  .SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("hosting.json", optional: true)
+                  .AddCommandLine(args)
+                  .Build();
+
+      return WebHost.CreateDefaultBuilder(args)
+        .UseConfiguration(configuration)
+          .UseStartup<Startup>()
+          .Build();
+    }
+  }
 }
